c2b45c73a78a30c67044d22bbdd4c723a8c9f7ef
Gustavo Pereira <gustavopereira233@gmail.com>
2019-08-20 13:00:25 -0300

    Updates

---
+++ precache-manifest.a8bab3ce6c7d934ea6970f425e2c3450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4faf0747021ddb038fb1b8801a0b4f18",
+    "url": "/dices/index.html"
+  },
+  {
+    "revision": "0822361d73baac5266a2",
+    "url": "/dices/static/css/main.bbbe14c0.chunk.css"
+  },
+  {
+    "revision": "667f15200bc877775b82",
+    "url": "/dices/static/js/2.77dfcf6e.chunk.js"
+  },
+  {
+    "revision": "0822361d73baac5266a2",
+    "url": "/dices/static/js/main.2dab0529.chunk.js"
+  },
+  {
+    "revision": "661861c00d2883aba8f9",
+    "url": "/dices/static/js/runtime~main.76abb68b.js"
+  }
+]);

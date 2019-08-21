dfa34de1900031897e60d1cb0a8bd07015fb8874
Gustavo Pereira <gustavopereira233@gmail.com>
2019-08-21 13:11:32 -0300

    Made some drastic changes to learn how to work with components

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/main';
+import AddListForm from './components/AddListForm';
 
 const Routes = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={Main} />
+            <Route exact path="/Form" component={AddListForm} />
         </Switch>
     </Router>
 );
